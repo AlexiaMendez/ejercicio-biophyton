82d308ee83d713e1bed035b60fd57e28867d540d
AlexiaMendez <72842423+AlexiaMendez@users.noreply.github.com>
2020-10-14 14:19:23 -0700

    Update Script.py

--- Script.py
+++ Script.py
@@ -1,2 +1,8 @@
-summarize_contents()
+from Bio.Seq import Seq
+from Bio.SeqFeatures import SeqFeature, FeatureLocation
+from Bio import SeqIO
+from Bio.SeqRecord import SeqRecord
+def summarize_contents(filename):
+        record = SeqIO.read ("filename", "genbank")
+        print(record)
 
