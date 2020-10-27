1a2a5d0e4dbbce041aa197673e3a1eb507c322bf
AlexiaMendez <72842423+AlexiaMendez@users.noreply.github.com>
2020-10-26 21:16:54 -0700

    Update Script.py

--- Script.py
+++ Script.py
@@ -3,5 +3,29 @@ from Bio.SeqFeatures import SeqFeature, FeatureLocation
 from Bio import SeqIO
 from Bio.SeqRecord import SeqRecord
 def summarize_contents(filename):
-        record = SeqIO.read ("filename", "genbank")
-        print(record)
+	listaOs = os.path.split(filename)
+	listaExt = os.path.splitext(filename)
+	if (listaExt[1] == ".gbk"):
+		type_file= "genbank"
+	else: 
+		type_file= "fasta"
+	record = list(SeqIO.parse(filename, type_file))
+	#Creacion de diccionario
+	d = {}
+	d['File:'] = listaOs[1]
+	d['Path:'] = listaOs[0]
+	d['Num_records:'] = len(record)
+	#Diccionario con listas
+	d['Names:'] = []
+	d['IDs:'] = []
+	d['Descriptions'] = []
+	#Registro de records
+	for seq_rcd in SeqIO.parse(filename,type_file):
+		d['Names:'].append(seq_rcd.name)
+		d['IDs:'].append(seq_rcd.id)
+		d['Descriptions'].append(seq_rcd.description)
+	return d
+#Imprimir la funcion
+if _name_ == "_main_":
+	resultados = summarize_contents(filename)
+	print(resultados)
